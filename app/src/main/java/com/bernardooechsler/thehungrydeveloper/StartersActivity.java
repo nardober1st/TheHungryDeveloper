f4c6bb49967502c6f0b6caefa1de0b21969e90cc
nardober1st <bernardo.oechsler6@gmail.com>
2023-02-21 22:22:30 -0800

    5th Commit

--- app/src/main/java/com/bernardooechsler/thehungrydeveloper/StartersActivity.java
+++ app/src/main/java/com/bernardooechsler/thehungrydeveloper/StartersActivity.java
@@ -3,6 +3,7 @@ package com.bernardooechsler.thehungrydeveloper;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class StartersActivity extends AppCompatActivity {
@@ -13,6 +14,22 @@ public class StartersActivity extends AppCompatActivity {
         setContentView(R.layout.activity_starters);
 
         ListView startersList = findViewById(R.id.list_view_starters);
+
+        Dish[] dishes = {
+                new Dish("Mushroom and tofu Maki", "Toasted seaweed wrapped around sushi rice, filled with chestnut mushroom and smoked tofu", 999),
+                new Dish("Melon and lemon soup", "Fresh melon and lemon combined into creamy soup", 1199),
+                new Dish("Coconut and chocolate mousse", "A creamy mousse made with fresh coconut and milk chocolate", 899),
+                new Dish("Spinach and cabbage wontons", "Thin wonton cases stuffed with fresh spinach and chinese cabbage", 799),
+                new Dish("Broccoli and cucumber soup", "Fresh broccoli and cucumber combined into creamy soup", 899),
+                new Dish("Chilli and aubergine dip", "A dip made from scotch bonnet chilli and fresh aubergine", 999),
+                new Dish("Chickpea and chilli gyoza", "Thin pastry cases stuffed with fresh chickpea and green chilli", 699),
+                new Dish("Sprout and pineapple soup", "Fresh sprout and pineapple combined into creamy soup", 899),
+                new Dish("Egusi and borscht soup", "Egusi and borscht combined into creamy soup", 1299)
+        };
+
+        ArrayAdapter<Dish> dishesAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, dishes);
+
+        startersList.setAdapter(dishesAdapter);
 
     }
 }
